2527bc5761556ac436356de6c7e4eda84d60e8c5
Alexander <sherstoboev93@gmail.com>
2023-06-15 11:53:03 +0300

    Scene settings added

--- Assets/Scripts/Block/Block.cs
+++ Assets/Scripts/Block/Block.cs
@@ -5,10 +5,14 @@ using UnityEngine.Events;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _destroyEffect;
     public event UnityAction<Block> BulletHit;
     public void Break()
     {
         BulletHit?.Invoke(this);
+        ParticleSystemRenderer renderer = Instantiate(_destroyEffect, transform.position,_destroyEffect.transform.rotation ).GetComponent<ParticleSystemRenderer>();
+        renderer.material.color = GetComponent<MeshRenderer>().material.color;
+
         Destroy(gameObject);
     }
 }
